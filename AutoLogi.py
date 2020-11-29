cd54e1620552b5cd12b0e5b9ae7ff685ef794c21
abdulrasheed-a <47692754+abdulrasheed-a@users.noreply.github.com>
2020-11-29 18:04:42 +0530

    Update AutoLogi.py

--- AutoLogi.py
+++ AutoLogi.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 print("---+ AutoLogi +---")
 print("******************")
 path="enter the driver path here"
-print(" 1.Facebook 2.Twitter \n 2.Help")
+print(" 1.Facebook 2.Twitter 3.Instagram  4.Help")
 n=int(input("Choose an option: "))
 if(n==1):
     print("AutoLogi is loading ...Please wait...")
@@ -24,8 +25,18 @@ elif(n==2):
     driver.get(url)
     driver.find_element_by_name("session[username_or_email]").send_keys(username)
     driver.find_element_by_name("session[password]").send_keys(password,Keys.RETURN)
+elif(n==3):
+    print("AutoLogi is loading ...Please wait...")
+    username='your_instagram_username'
+    password='your_instagram_pass' 
+    url='https://instagram.com/'
+    driver=webdriver.Chrome(path)
+    driver.get(url)
+    time.sleep(3)
+    driver.find_element_by_name("username").send_keys(username)
+    driver.find_element_by_name("password").send_keys(password,Keys.RETURN)
 else:
     print("Need Help?")
     print("Here are some solutions\n")
     print("->Make sure you have downloaded the appropriate driver of your browser and put it in the given driver path\n")
-    print("->Websites may have changed the ids of the login box, then change it in the program and please report us\n")
+    print("->Websites may have changed the ids of the login box, then change it in the program and please open an issue\n")
